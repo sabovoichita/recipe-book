1423efd5899904afb996047db6eec27a1d3bd159
Voichita Maria <sabo_voichita@yahoo.com>
2024-09-27 12:45:14 +0200

    create input area and delete/edit buttons

--- src/index.js
+++ src/index.js
@@ -22,7 +22,14 @@ function renderRecipe(recipe) {
         <img src="${recipe.image}" alt="${recipe.title}" loading="lazy" />
         <h2>${recipe.title}</h2>
         <p><strong>Ingredients:</strong> ${recipe.ingredients}</p>
+        <div id = "endSection">
         <a href="${recipe.link}" target="_blank">View Recipe</a>
+        <div id="deleteEdit">
+        <button type="button" class="deleteBtn actionBtn">♻</button>
+        <span id="spacer"></span>
+        <button type="button" class="editBtn actionBtn">✖</button>
+        </div>
+        </div>
       </li>
     `;
 }
@@ -39,6 +46,14 @@ function createHTMLStructure() {
         <h1>Recipe Book</h1>
       </header>
       <div class="recipesContainer">
+          <div id="inputArea">
+            <input type="text" name="" id="idInput" placeholder="Enter id" />
+            <input type="text" name="" id="titleInput" placeholder="Enter title" />
+            <input type="file" name="" id="imageInput" />
+            <input type="text" name="" id="ingredinentsInput" placeholder="Enter ingredients" />
+            <input type="url" name="" id="linkInput" placeholder="Enter link" />
+            
+          </div>
         <ul id="recipeList" class="recipeList"></ul>
       </div>
     `;
